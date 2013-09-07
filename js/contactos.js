c404cdeaf016c54ed254b5293da4531a08d0fe9d
abelobog <abelobog@hotmail.com>
2013-09-07 12:54:01 -0500

    3

--- js/contactos.js
+++ js/contactos.js
@@ -1,1 +1,42 @@
 //Contactos
+
+$(function(){
+    $('#cListar').tap(function(){
+        leerContactos();
+    });
+});
+
+
+// Wait for device API libraries to load
+function leerContactos(){
+    document.addEventListener("deviceready", contactos, false);
+}
+
+// device APIs are available
+
+function contactos() {
+    // find all contacts with 'Bob' in any name field
+    var options = new ContactFindOptions();
+    var fields = ["*"];
+    navigator.contacts.find(fields, leidos, onError1, options);
+}
+
+// onSuccess: Get a snapshot of the current contacts
+
+function leidos(contacts) {
+    if (contacts.length > 0)
+        $('#cMostrar').html('');
+    else
+        $('#cMostrar').html('<li>Sin leer contactos</li>');
+        for (var i = 0; i < contacts.length; i++) {
+            var tel = contacts[i].phoneNumbers[0].value;
+            var nombre = contacts[i].name.formatted;
+            $('#cMostrar').append('<li><a href="tel: ' + tel + '">'+ nombre  +'</a></li>');
+    }
+}
+
+// onError: Failed to get the contacts
+
+function onError1(contactError) {
+    alert('onError!');
+}
